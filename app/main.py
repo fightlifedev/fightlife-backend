32ea1b252165d670a7360d8d82c29af4497cfaed
fightlifedev <fixiveyt@gmail.com>
2026-06-30 10:52:47 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from routes.players import router as player_router
-from routes.world import router as world_router
+from app.routes.players import router as player_router
+from app.routes.world import router as world_router
 
 app = FastAPI()
 
 app.include_router(player_router)
 app.include_router(world_router)
-
 
 @app.get("/")
 def root():
