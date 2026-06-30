d5bb6b368da52fe46aea7e564a766e3cd5670e52
fightlifedev <fixiveyt@gmail.com>
2026-06-30 10:13:00 -0400

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config import MONGO_URI, DATABASE_NAME
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client[DATABASE_NAME]
+
+players_collection = db["players"]
+world_collection = db["world"]
