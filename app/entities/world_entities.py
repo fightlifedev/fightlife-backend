33d12766c37010c875584abe877c458edee3d06a
fightlifedev <fixiveyt@gmail.com>
2026-07-01 16:51:08 -0400

    Update world_entities.py

--- app/entities/world_entities.py
+++ app/entities/world_entities.py
@@ -1,16 +1,7 @@
 from app.entities.fighters import fighters
 from app.entities.fans import fans
-
-try:
-    from app.entities.promoters import promoters
-except ImportError:
-    promoters = []
-
-try:
-    from app.entities.media import media
-except ImportError:
-    media = []
-
+from app.entities.promoters import promoters
+from app.entities.media import media
 
 world_entities = (
     fighters +
