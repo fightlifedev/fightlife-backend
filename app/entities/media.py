62fdd245730f105060a48b1ddf14f00ebb4cffb4
fightlifedev <fixiveyt@gmail.com>
2026-07-01 16:56:21 -0400

    Update media.py

--- app/entities/media.py
+++ app/entities/media.py
@@ -1,1 +1,20 @@
-
+media = [
+    {
+        "name": "MMAWorldDaily",
+        "type": "media",
+        "followers": 220000,
+        "verified": True
+    },
+    {
+        "name": "ESPN MMA",
+        "type": "media",
+        "followers": 5400000,
+        "verified": True
+    },
+    {
+        "name": "UFC News Hub",
+        "type": "media",
+        "followers": 125000,
+        "verified": False
+    }
+]
