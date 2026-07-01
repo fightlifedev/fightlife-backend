81aa8c443a9c3e3cbdb10123a8fcef639dd95383
fightlifedev <fixiveyt@gmail.com>
2026-07-01 14:02:20 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
+
 from app.routes.players import router as player_router
 from app.routes.world import router as world_router
 from app.routes import contracts
+from app.routes import cagewire
 
 app = FastAPI()
+
+# ============================
+# ROUTERS
+# ============================
 
 app.include_router(player_router)
 app.include_router(world_router)
 app.include_router(contracts.router)
+app.include_router(cagewire.router)
+
+
+# ============================
+# ROOT
+# ============================
 
 @app.get("/")
 def root():
-    return {"message": "FightLife API Running"}
+    return {
+        "message": "FightLife API Running"
+    }
