00253d88494acf9f8a04e5662a68cf678f047bc2
fightlifedev <fixiveyt@gmail.com>
2026-07-01 13:46:27 -0400

    Create world_entities.py

---
+++ app/entities/world_entities.py
@@ -0,0 +1,25 @@
+from app.entities.fighters import fighters
+
+# Future imports
+# from app.entities.rappers import rappers
+# from app.entities.actors import actors
+# from app.entities.brands import brands
+# from app.entities.media import media
+# from app.entities.streamers import streamers
+# from app.entities.cities import cities
+# from app.entities.states import states
+# from app.entities.poi import poi
+
+world_entities = {
+    "fighters": fighters,
+
+    "rappers": [],
+    "actors": [],
+    "brands": [],
+    "media": [],
+    "streamers": [],
+    "athletes": [],
+    "cities": [],
+    "states": [],
+    "poi": []
+}
