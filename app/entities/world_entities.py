3bf04d0682505c7bf5f2a3e38ae1b08c0f92d6ce
fightlifedev <fixiveyt@gmail.com>
2026-07-01 15:04:30 -0400

    Update world_entities.py

--- app/entities/world_entities.py
+++ app/entities/world_entities.py
@@ -1,4 +1,5 @@
 from app.entities.fighters import fighters
+from app.entities.fans import fans
 
 # Future imports
 # from app.entities.rappers import rappers
@@ -9,17 +10,37 @@ from app.entities.fighters import fighters
 # from app.entities.cities import cities
 # from app.entities.states import states
 # from app.entities.poi import poi
+# from app.entities.civilians import civilians
+# from app.entities.sponsors import sponsors
+# from app.entities.companies import companies
+
+
+# ============================
+# MASTER WORLD ENTITY REGISTRY
+# FightLife Universe Core
+# ============================
 
 world_entities = {
     "fighters": fighters,
+    "fans": fans,
 
+    # Future social/cultural layers
     "rappers": [],
     "actors": [],
     "brands": [],
     "media": [],
     "streamers": [],
     "athletes": [],
+
+    # Geography layers
     "cities": [],
     "states": [],
-    "poi": []
+    "poi": [],
+
+    # Civilian world
+    "civilians": [],
+
+    # Business/economy layers
+    "sponsors": [],
+    "companies": []
 }
