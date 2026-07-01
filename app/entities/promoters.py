5bb47f0bc90a82a08e333e03a2fdc2ef7b59a6d9
fightlifedev <fixiveyt@gmail.com>
2026-07-01 16:56:03 -0400

    Update promoters.py

--- app/entities/promoters.py
+++ app/entities/promoters.py
@@ -1,1 +1,20 @@
-
+promoters = [
+    {
+        "name": "Dana White",
+        "type": "promoter",
+        "followers": 8100000,
+        "verified": True
+    },
+    {
+        "name": "Joe Rogan",
+        "type": "promoter",
+        "followers": 18000000,
+        "verified": True
+    },
+    {
+        "name": "Ariel Helwani",
+        "type": "promoter",
+        "followers": 2500000,
+        "verified": True
+    }
+]
