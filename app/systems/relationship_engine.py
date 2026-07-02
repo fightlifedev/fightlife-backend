8c226521b77f4b90af1cb13823312dabd50b7d34
fightlifedev <fixiveyt@gmail.com>
2026-07-01 22:48:50 -0400

    Update relationship_engine.py

--- app/systems/relationship_engine.py
+++ app/systems/relationship_engine.py
@@ -127,3 +127,13 @@ def should_attack(entity, target):
         return True
 
     return False
+def should_attack(entity, target):
+    init_relationship(entity)
+    entity_id = ensure_handle(entity)
+
+    ensure_handle(target)
+
+    if target["handle"] in relationships[entity_id]["rivals"]:
+        return True
+
+    return False
