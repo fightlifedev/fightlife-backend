f8d5267a102f819041c57aa5c4fb35d100521aba
fightlifedev <fixiveyt@gmail.com>
2026-07-02 11:50:58 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from app.routes.life import router as life_router
 from app.routes.players import router as player_router
 from app.routes.world import router as world_router
 from app.routes import contracts
@@ -15,7 +15,7 @@ app.include_router(player_router)
 app.include_router(world_router)
 app.include_router(contracts.router)
 app.include_router(cagewire.router)
-
+app.include_router(life_router, prefix="/life", tags=["Life"])
 
 # ============================
 # ROOT
